34158e30f1b88ef3c9d1dff0b6d643ee1fd11a4d
raimondseisaks <eisaks83@gmail.com>
2024-05-22 00:25:57 +0300

    Commit message

--- InternshipTasks/Day2/src/main/scala/Testing.scala
+++ InternshipTasks/Day2/src/main/scala/Testing.scala
@@ -1,5 +1,26 @@
-object Hello {
-  def main(args: Array[String]) = {
-    println("Hello, world")
+import scala.io.StdIn.readInt
+import scala.io.StdIn.readDouble
+import scala.io.StdIn.readLine
+
+case class driving(speed: Double, place: String){
+  def isOverSpeeding() : Unit =
+    if (speed > 90) println("Sods 50 eiro") else println("Tu esi losis")
+}
+
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    // Reading a string
+    println("Speed: ")
+    val speed = readDouble()
+
+    // Reading an integer
+    println("Enter a place of driving: ")
+    val age = readLine()
+
+    val user = new driving(speed, age)
+    user.isOverSpeeding()
+    // Reading a double
+    //println(s"Your height is $height meters.")
   }
-}//š
+}
